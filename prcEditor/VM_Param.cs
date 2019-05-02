af02f4d0a2846421b96e073db54916574aa0032e
BenHall-7 <benjaminjahall@gmail.com>
2019-05-02 01:30:31 -0400

    Remove and sort usings

--- prcEditor/VM_Param.cs
+++ prcEditor/VM_Param.cs
@@ -1,11 +1,6 @@
 ﻿using paracobNET;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace prcEditor
 {
